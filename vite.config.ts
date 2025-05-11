9505962cd6e66a30dc275ddd6958d57dc2d62f40
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2025-05-11 11:22:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  },
 });
